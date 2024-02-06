683e4e61586754b6b47ba37911378572f2666393
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-02-05 20:21:12 -0600

    Updated color scheme

--- lua/omar/plugins/colorscheme.lua
+++ lua/omar/plugins/colorscheme.lua
@@ -1,48 +1,47 @@
 return {
-  {
-    "bluz71/vim-nightfly-guicolors",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
-    end,
-  },
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     local bg = "#011628"
-  --     local bg_dark = "#011423"
-  --     local bg_highlight = "#143652"
-  --     local bg_search = "#0A64AC"
-  --     local bg_visual = "#275378"
-  --     local fg = "#CBE0F0"
-  --     local fg_dark = "#B4D0E9"
-  --     local fg_gutter = "#627E97"
-  --     local border = "#547998"
-  --
-  --     require("tokyonight").setup({
-  --       style = "night",
-  --       on_colors = function(colors)
-  --         colors.bg = bg
-  --         colors.bg_dark = bg_dark
-  --         colors.bg_float = bg_dark
-  --         colors.bg_highlight = bg_highlight
-  --         colors.bg_popup = bg_dark
-  --         colors.bg_search = bg_search
-  --         colors.bg_sidebar = bg_dark
-  --         colors.bg_statusline = bg_dark
-  --         colors.bg_visual = bg_visual
-  --         colors.border = border
-  --         colors.fg = fg
-  --         colors.fg_dark = fg_dark
-  --         colors.fg_float = fg
-  --         colors.fg_gutter = fg_gutter
-  --         colors.fg_sidebar = fg_dark
-  --       end,
-  --     })
-  --     -- load the colorscheme here
-  --     vim.cmd([[colorscheme tokyonight]])
-  --   end,
-  -- },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			require("tokyonight").setup({
+				-- THEME VARIATION
+				style = "night",
+				-- Telescope borderless
+				on_highlights = function(hl, c)
+					local prompt = "#2d3149"
+					hl.TelescopeNormal = {
+						bg = c.bg_dark,
+						fg = c.fg_dark,
+					}
+					hl.TelescopeBorder = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+					hl.TelescopePromptNormal = {
+						bg = prompt,
+					}
+					hl.TelescopePromptBorder = {
+						bg = prompt,
+						fg = prompt,
+					}
+					hl.TelescopePromptTitle = {
+						bg = prompt,
+						fg = prompt,
+					}
+					hl.TelescopePreviewTitle = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+					hl.TelescopeResultsTitle = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+				end,
+			})
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 }
