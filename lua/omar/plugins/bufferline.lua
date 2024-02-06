0a058daf35c07f0b3a1fa2b3773b44709e273bf4
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-02-05 20:25:49 -0600

    Updated UI and animations added

--- lua/omar/plugins/bufferline.lua
+++ lua/omar/plugins/bufferline.lua
@@ -1,11 +1,18 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	event = "VeryLazy",
+	keys = {
+		{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+	},
+	opts = {
+		options = {
+			mode = "tabs",
+			separator_style = "slant",
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+		},
+	},
 }
