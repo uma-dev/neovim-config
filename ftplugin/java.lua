25891004c4131f84b6aee1113270a01d30ea64da
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-04-03 15:41:03 -0600

    fix(lsp-java): deactivate jdtls double instance

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -50,11 +50,11 @@ local on_attach = function(client, bufnr)
 	keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 end
 
-local config = {
-	capabilities = capabilities,
-	on_attach = on_attach, -- We pass our on_attach keybindings to the configuration map
-
-	cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-}
-require("jdtls").start_or_attach(config)
+-- local config = {
+-- 	capabilities = capabilities,
+-- 	on_attach = on_attach, -- We pass our on_attach keybindings to the configuration map
+--
+-- 	cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
+-- 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+-- }
+-- require("jdtls").start_or_attach(config)
